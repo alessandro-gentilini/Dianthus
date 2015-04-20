18daecb47c2f968facc7abac73ab9f14e93ef63d
Alessandro Gentilini <alessandro.gentilini@gmail.com>
2015-04-20 21:55:00 +0200

    Clustering is done.

--- dianthus.cpp
+++ dianthus.cpp
@@ -2,6 +2,7 @@
 #include "opencv2/highgui/highgui.hpp"
 
 #include <iostream>
+#include <vector>
 
 int main(int argc,char** argv)
 {
@@ -35,10 +36,52 @@ int main(int argc,char** argv)
 	cv::namedWindow("binary");
 	cv::imshow("binary",binary); 
 
-	cv::Mat points;
-	cv::bitwise_and(sampling,binary,points);
-	cv::namedWindow("points");
-	cv::imshow("points",points);	
+	cv::Mat img_points;
+	cv::bitwise_and(sampling,binary,img_points);
+	cv::namedWindow("img_points");
+	cv::imshow("img_points",img_points);
+
+	std::vector< cv::Point > points;
+
+	for ( size_t c = 0; c < img_points.cols; c++ ) {
+		for ( size_t r = 0; r < img_points.rows; r++ ) {
+			if (img_points.at<unsigned char>(r,c)) {
+				points.push_back(cv::Point(c,r));
+			}
+		}
+	}
+
+	cv::Mat samples(points.size(),2,CV_32F);
+	for ( size_t i = 0; i < points.size(); i++ ) {
+		samples.at<float>(i,0)=points[i].x;
+		samples.at<float>(i,1)=points[i].y;
+	}
+
+
+
+	std::vector<int> labels;
+
+	int attempts = 5;
+  	cv::Mat centers;
+	cv::kmeans(samples,26+10,labels,cv::TermCriteria(CV_TERMCRIT_ITER|CV_TERMCRIT_EPS, 10000, 0.0001),attempts, cv::KMEANS_PP_CENTERS, centers);
+
+	
+
+	cv::Mat clusters = cv::Mat::zeros( ground_truth.rows, ground_truth.cols, CV_8UC3 );
+
+
+	cv::RNG rng(12345);
+	std::vector< cv::Vec3b > colors;
+	for ( size_t i = 0; i < 26+10; i++ ) {
+		colors.push_back(cv::Vec3b(rng.uniform(0,255), rng.uniform(0, 255), rng.uniform(0, 255)));
+	}
+
+
+	for ( size_t i = 0; i < labels.size(); i++ ) {
+		clusters.at<cv::Vec3b>(points[i]) = colors[labels[i]];
+	}
+	cv::namedWindow("clusters");
+	cv::imshow("clusters",clusters);	
 
 	cv::waitKey(0);
 
